7bd52a9e95b0581b96385b3fee22b7085c8a7c78
amata7 <adrianmata@live.com>
2021-03-19 16:20:49 -0700

    checks which character types are allowed, and adds their respective characters into the pwChoices array

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,81 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-const resultEl = document.getElementById('result');
-const lengthEl = document.getElementById('length');
-const uppercaseEl = document.getElementById('uppercase');
-const lowercaseEl = document.getElementById('lowercase');
-const numberEl = document.getElementById('number');
-const symbolEl = document.getElementById('symbol');
-// const generateEl = document.getElementById('generate');
-// const clipboardEl = document.getElementById('clipboard');
+var pwArray = [];
+var pwChoices = [];
+var symbols = " !@#$%^&*(){}[]-_+|:;'?/.,<=>`~".split("");
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var lower = "abcdefghijklmnopqrstuvwxyz".split("");
+var numbers = "0123456789".split("");
+// Write password to the #password input
 
-const  random = {
-  lower:randomLow,
-  upper:randomUp,
-  number:randomNum,
-  symbol:randomSymbol
+function generatePassword() {
+  
+  let generatedPassword = "";
+  let chosenLength = prompt("Choose a number between 8 and 128.");
+    if (chosenLength < 8) {
+      alert('Password must contain more charcters')
+      return "";
+    }
+
+    if (chosenLength > 128) {
+      alert('Password must contain less charcters')
+      return "";
+    }
+  let alertLength = alert('Your password will be ' + chosenLength + ' characters long.');
+
+  let allowSymbol = window.confirm("Do you want your password to contain special characters?");
+
+  let allowNumber = window.confirm("Do you want your password to contain numbers?");
+
+  let allowUpper = window.confirm("Do you want your password to contain uppercase characters?");
+
+  let allowLower = window.confirm("Do you want your password to contain lowercase characters?");
+
+  var allowable = [allowSymbol, allowNumber, allowUpper, allowLower];  
+  // var allowableNum = allowSymbol + allowNumber + allowUpper + allowLower;
+  // var charTypes = [{allowLower}, {allowUpper}, {allowNumber}, {allowSymbol}].filter(item => Object.values(item)[0]);
+
+    if (!allowable.includes(true)) {
+      window.alert("Choose at least one type of character to use.");
+      return "";
+    }
+    // If the user chooses to allow uppercase then run this code, adds uppercase char to possible type
+    if (allowUpper === true) {
+      pwChoices = pwChoices.concat(upper);
+    }
+
+    if (allowLower === true) {
+      pwChoices = pwChoices.concat(lower);
+    }
+
+    if (allowNumber === true) {
+      pwChoices = pwChoices.concat(numbers);
+    }
+
+    if (allowSymbol === true) {
+      pwChoices = pwChoices.concat(symbols);
+    }
+
+
+    console.log(pwChoices);
+    console.log(typeof pwChoices);
+return "Password";
 }
 
-// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 function randomLow() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
@@ -35,55 +93,4 @@ function randomSymbol() {
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
-function generatePassword() {
-  let chosenLength = prompt("Choose a number between 8 and 128.");
-  if (chosenLength > 7 && chosenLength < 129) {
-    let alertLength = alert('Your password will be ' + chosenLength + ' characters long.');
-    let allowSymbol = window.confirm("Do you want your password to contain special characters?");
-    allowSymbol.type = "boolean"
-
-    let allowNumber = window.confirm("Do you want your password to contain numbers?");
-    allowNumber.type = "boolean"
-
-    let allowUpper = window.confirm("Do you want your password to contain uppercase characters?");
-    allowUpper.type = "boolean"
-
-    let allowLower = window.confirm("Do you want your password to contain lowercase characters?");
-    allowLower.type = "boolean"
-
-    var allowable = [allowSymbol, allowNumber, allowUpper, allowLower];   
-
-    console.log(allowable);
-    console.log(allowSymbol);
-    console.log(allowNumber);
-    console.log(allowUpper);
-    console.log(allowLower);
-    //The below code does not work as intended, needs some more work
-    if (allowSymbol = "true") {
-      
-      window.alert("You got to this point");
-    }
-    else {
-      window.alert("Please choose at least one character type");
-    return;
-    }
-  }
-  else {
-    window.alert("Please choose a valid number");
-    return;
-  }
-
-}
-
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// var small = "abcdefggkjhkf".split("");
